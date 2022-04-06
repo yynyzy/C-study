ce9e001eec9dfab9c7cc9db6e9bd378b5f3c69b1
yyn <1601530253@qq.com>
2022-04-06 23:26:22 +0800

    4-6

--- 06-结构体/01-类型定义.c
+++ 06-结构体/01-类型定义.c
@@ -8,6 +8,23 @@ int main(){
     typedef char CHAR;
     CHAR c ='c';
     printf("%c\n",c);
-    return 0 ;
 
+//结构体
+    struct 
+    {
+       int x;
+       int y;
+    }point;
+    point.x =10;
+    point.y =10;
+    printf("%d,%d\n",point.x,point.y);
+    
+//自定义结构体名字来简化结构体名字
+ typedef struct {int x;int y;}Point;
+    Point point2;
+    point2.x =20;
+    point2.y =20;
+    printf("%d,%d\n",point2.x,point2.y);
+
+    return 0 ;
 }
