d031bfc8124a1220cf8784953d03a0baca507350
yanyinuo <1601530253@qq.com>
2022-04-11 20:06:44 +0800

    4-11

--- 06-结构体/01-类型定义.c
+++ 06-结构体/01-类型定义.c
@@ -2,36 +2,55 @@
 
 //给结构体换名字来简化结构体名字
 //在全局定义，可以在所有函数中使用
- typedef struct 
- {
-     int x;
- int y;
- }Point;
-int main(){
+/*
+在定义结构体类型的时候顺便定义结构体变量，以后还可以定义结构体变量struct结构体类型名
+
+struct 结构体类型名
+{
+    成员列表;
+
+}结构体变量1,变量2;
+struct结构体类型名 变量3,变量4;
+
+*例子
+struct stu ;
+{   //定义了三个struct stu类型的变量
+    //每个变量都有三个成员，分别是num，name，sex
+    int num;
+    char name[20];
+    char sex;
+} lucy,bob,lilie; //结构体变量，分别是lucy，bob，lilie
+*/
+typedef struct
+{
+    int x;
+    int y;
+} Point;
+int main()
+{
     // typedef:用来给数据类型取新的名字
     typedef int MYINT;
-    MYINT a =10;
-    printf("%d\n",a);// 如果不是 int型，输出会报错
+    MYINT a = 10;
+    printf("%d\n", a); // 如果不是 int型，输出会报错
 
     typedef char CHAR;
-    CHAR c ='c';
-    printf("%c\n",c);
+    CHAR c = 'c';
+    printf("%c\n", c);
 
-//结构体
-    struct 
+    //结构体
+    struct
     {
-       int x;
-       int y;
-    }point;
-    point.x =10;
-    point.y =10;
-    printf("%d,%d\n",point.x,point.y);
-    
+        int x;
+        int y;
+    } point;
+    point.x = 10;
+    point.y = 10;
+    printf("%d,%d\n", point.x, point.y);
 
     Point point2;
-    point2.x =20;
-    point2.y =20;
-    printf("%d,%d\n",point2.x,point2.y);
+    point2.x = 20;
+    point2.y = 20;
+    printf("%d,%d\n", point2.x, point2.y);
 
-    return 0 ;
+    return 0;
 }
--- 08-格式化输出/01-printf.c
+++ 08-格式化输出/01-printf.c
@@ -5,7 +5,12 @@ int main()
     float b = 1.1;
     char c = 'C';
     char str[10] = "string";
-    char str1[6] = "string";//6空间不够，会出现乱码
+    char str1[6] = "string";
+    /*
+    str[6]会导致空间不够，会出现乱码。
+    原因是：
+    字符串本质上其实就是以'\0作为'结尾的特殊字符数组，所以定义字符串时，必须保证字符串存储的最后一个元素为'\0'。字符串长度等于字符串实际长度+1
+    */
     printf("int var : %d\n", a);
     printf("float var:%f\n", b);
     printf("char var: %c\n", c);
