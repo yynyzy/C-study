a630b57f347e85ccfbf4ff27494bc53987d3fadf
yanyinuo <1601530253@qq.com>
2022-04-22 20:45:00 +0800

    4-22

---
+++ 13-各类数据结构特点/顺序表建表.cpp
@@ -0,0 +1,12 @@
+/*
+int A[maxSize];
+int length;
+int createList(int A[], int &length)
+{
+    cin >> length;
+    if (length > maxSize)
+        return 0;
+    for (int i = 0; i < length; ++i)
+        cin >> A[i];
+    return 1;
+*/
