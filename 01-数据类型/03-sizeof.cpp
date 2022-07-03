8fd367c7b092b57f012e7d7b274f35988728b91e
yyn <1601530253@qq.com>
2022-07-03 22:01:57 +0800

    7-3

---
+++ 01-数据类型/03-sizeof.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<<sizeof(short)<<endl; //2
+    cout<<sizeof(int)<<endl;    //4
+    cout<<sizeof(long)<<endl;   //8
+    cout<<sizeof(long long)<<endl;  //8
+    return 0;
+}
